4268285d2599025472e3c5a24083615d97d8c750
stefanholzapfel <Nafets123>
2014-11-29 15:28:23 +0100

    Shared objects added

--- Shared/Interfaces/ICalendarSyncable.cs
+++ Shared/Interfaces/ICalendarSyncable.cs
@@ -6,7 +6,31 @@ using System.Threading.Tasks;
 
 namespace Shared.Interfaces
 {
-    class ICalendarSyncable
+    /// <summary>
+    /// Interface for interacting with any calendar (Outlook or remote connector)
+    /// </summary>
+    public interface ICalendarSyncable
     {
+        /// <summary>
+        /// returns the updates in the calendar since a timestamp
+        /// </summary>
+        /// <param name="calendarName">name of calendar</param>
+        /// <param name="timestamp">since when the changes should be returned</param>
+        /// <returns>appointments that have been updated</returns>
+        AppointmentSyncCollection GetUpdates(String calendarName, DateTime timestamp);
+
+        /// <summary>
+        /// returns the full calendar
+        /// </summary>
+        /// <param name="calendarName">name of calendar</param>
+        /// <returns>all appointments as 'add'</returns>
+        AppointmentSyncCollection GetUpdates(String calendarName);
+
+        /// <summary>
+        /// applies the updates to the calendar
+        /// </summary>
+        /// <param name="calendarName">name of calendar</param>
+        /// <param name="syncItems">appointments to be updated</param>
+        void DoUpdates(String calendarName, AppointmentSyncCollection syncItems);
     }
 }
