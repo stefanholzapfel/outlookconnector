e9d41836175ebda3f7a6b4bc47a6618c1f6a6ada
Fuermi <andreas.fuermann@gmail.com>
2014-12-26 21:37:56 +0100

    ConfigManager added

---
+++ ConfigManager/ConfigurationManager.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security.Cryptography;
+
+
+namespace ConfigManager
+{
+    public class ConfigurationManager
+    {
+        Config conf = new Config();
+
+        private static byte[] entropy = { 5, 19, 22, 8, 16, 27, 35, 65, 91 };
+        private static string filename = @"config";
+
+        public ConfigurationManager()
+        {
+            FileManager fileMan = new FileManager();
+            if (fileMan.LoadXML<Config>(filename) != null)
+            {
+                conf = fileMan.LoadXML<Config>(filename);
+            }
+        }
+        /// <summary>
+        /// Get the current configuration without encrypted password.
+        /// To get decrypted password use GetPassword method.
+        /// </summary>
+        /// <returns>Object of type Config</returns>
+        public Config GetConfig()
+        {
+            if (conf != null)
+            {
+                return conf;
+            }
+            else
+                return null;
+        }
+        /// <summary>
+        /// Get the current password.
+        /// </summary>
+        /// <returns>Password of type String or null if there is no password set</returns>
+        public String GetPassword()
+        {
+            if (conf.password != null)
+            {
+                byte[] password = ProtectedData.Unprotect(conf.password, entropy, DataProtectionScope.CurrentUser);
+                return Encoding.UTF8.GetString(password);
+            }
+            else
+                return null;
+        }
+        /// <summary>
+        /// Set the synced Parameter in the config.xml
+        /// </summary>
+        /// <param name="_synced">0 = not synced, 1 = synced</param>
+        public void SetSynced(byte _synced)
+        {
+            conf.synced = _synced;
+            SaveConfig();
+        }
+        public void SetConfig(string _userName, string _password, string _claendarName, string _connector, string _URL, int _updateInterval, byte _synced)
+        {
+
+            conf.userName = _userName;
+            conf.password = Protect(Encoding.UTF8.GetBytes(_password));
+            conf.calendarName = _claendarName;
+            conf.connector = _connector;
+            conf.URL = _URL;
+            conf.updateInterval = _updateInterval;
+            conf.synced = _synced;
+            SaveConfig();
+        }
+        public void SaveConfig()
+        {
+            FileManager fileMan = new FileManager();
+            fileMan.SaveXML(conf, filename);        
+          
+        }        
+        public static byte[] Protect(byte[] data)
+        {
+            try
+            {
+                return ProtectedData.Protect(data, entropy, DataProtectionScope.CurrentUser);
+            }
+            catch (CryptographicException e)
+            {
+                Console.WriteLine("Data was not encrypted. An error occurred.");
+                Console.WriteLine(e.ToString());
+                return null;
+            }
+        }
+    }   
+    public class Config
+    {
+        public string userName { get; set; }
+        public byte[] password { get; set; }
+        public string calendarName { get; set; }
+        public string connector { get; set; }
+        public string URL { get; set; }
+        public int updateInterval { get; set; }
+        public byte synced { get; set; }
+        
+    }
+}
---
+++ ConfigManager/FileManager.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace ConfigManager
+{
+    public class FileManager
+    {
+        private static string path = @"C:\Users\"+Environment.UserName+@"\AppData\Roaming\Microsoft\Outlook\OutlookConnector\";
+        /// <summary>
+        /// XML Serializer to save a generic object.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="obj">Any object you want to save as xml</param>
+        /// <param name="Filename">Filename of the xml.</param>
+        public void SaveXML<T>(T obj, string filename)
+        {
+            Directory.CreateDirectory(path);            
+            using (var fileStream = new FileStream(path+filename+".xml", FileMode.Create))
+            {
+                var ser = new XmlSerializer(typeof(T));
+                ser.Serialize(fileStream, obj);
+            }
+
+        }
+
+        private void CreateDictionary()
+        {
+            throw new NotImplementedException();
+        }
+        /// <summary>
+        /// XML Serializer to load a generic object.
+        /// </summary>
+        /// <typeparam name="T">Any object you want to from an xml file.</typeparam>
+        /// <param name="Filename">Filename of the xml.</param>
+        /// <returns>Returns any spezified object with the xml data.</returns>
+        public T LoadXML<T>(string filename)
+        {
+            T result;
+
+            if (Directory.Exists(path))
+            {
+                var ser = new XmlSerializer(typeof(T));
+                try
+                {
+                    using (var tr = new StreamReader(path + filename + ".xml"))
+                    {
+                        result = (T)ser.Deserialize(tr);
+                        return result;
+                    }
+                }
+                catch (FileNotFoundException ex)
+                {
+                    return result = default(T);
+                }
+            }
+            else
+                return result = default(T);
+
+        }
+    }
+}
