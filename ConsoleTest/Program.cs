a7a80135216431bfa682a6c2d1a40283acfe5b06
hoebbelsB <hoebbelsb@gmail.com>
2015-01-06 19:48:50 +0100

    ups

--- ConsoleTest/Program.cs
+++ ConsoleTest/Program.cs
@@ -15,7 +15,7 @@ namespace ConsoleTest
             ConnectorHandler myHandler = new ConnectorHandler();
 
             myHandler.ChooseConnector("CaldavConnector");
-            myHandler.Settings = new ConnectorSettings("fst5", "fst5", "https://nas.apfelstrudel.net/owncloud/remote.php/caldav/calendars/fst5/fst5");
+            myHandler.Settings = new ConnectorSettings("fst5", "fst5", "stefan bitte ausbbessern");
 
             AppointmentSyncCollection test = myHandler.GetUpdates();
 
